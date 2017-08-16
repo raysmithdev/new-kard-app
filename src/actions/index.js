8df4d4311781e1629a2a4de6b19a36a05f0a81e9
tckastanek <tckastanek@gmail.com>
2017-08-15 22:30:25 -0400

    Wired login and signup together to be handled by the same submit.

--- src/actions/index.js
+++ src/actions/index.js
@@ -25,19 +25,28 @@ export const getInitialView = () => dispatch => {
   });
 };
 
-export const firebaseSignup = (loginEmail, loginPassword) => {
-  firebase.auth()
-    .createUserWithEmailAndPassword(loginEmail, loginPassword)
-    .catch((err) => {
-      console.log(`${err.code}: ${err.message}`);
-    });
-};
-
 export const firebaseLogin = (loginEmail, loginPassword) => {
   firebase.auth()
     .signInWithEmailAndPassword(loginEmail, loginPassword)
     .catch((err) => {
       console.log(`${err.code}: ${err.message}`);
+    });
+};
+
+/*
+*  The below might be an antipattern, conflating login and signup. Will be
+*  switching to OAuth anyway.
+*/
+export const firebaseSignup = (loginEmail, loginPassword) => {
+  firebase.auth()
+    .createUserWithEmailAndPassword(loginEmail, loginPassword)
+    .catch((err) => {
+      if (err.code === 'auth/email-already-in-use') {
+        firebaseLogin(loginEmail, loginPassword);
+      }
+      else {
+        console.log(`${err.code}: ${err.message}`);
+      }
     });
 };
 
